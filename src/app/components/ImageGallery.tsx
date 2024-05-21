6e5766494327278b85a0df8b319dfaa2a5375c5a
LEELAMANOHARGUDIVADA <leelamanohar.gudivada@gmail.com>
2024-05-21 13:46:12 +0530

    Fixed errors in ImageGallery

--- src/app/components/ImageGallery.tsx
+++ src/app/components/ImageGallery.tsx
@@ -18,7 +18,7 @@ export default function ImageGallery({images}: iAppProps) {
   return (
     <div className="grid gap-4 lg:grid-cols-5">
         <div className="order-last flex gap-4 lg:order-none lg:flex-col">
-            {images.map((image: any, index) => (
+            {images.map((image: any, index: any) => (
                 <div key={index} className="overflow-hidden rounded-lg bg-gray-100">
                     <Image src={urlFor(image).url()} width={200} height={200} alt="image" className="h-full w-full object-contain cursor-pointer"  onClick={() => handleSmallImageClick(image)} />
                 </div>
