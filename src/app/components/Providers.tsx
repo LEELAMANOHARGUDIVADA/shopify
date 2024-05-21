9c3a4f7128d687e402b72be241dc2915cd6a2bd8
LEELAMANOHARGUDIVADA <leelamanohar.gudivada@gmail.com>
2024-05-21 14:02:25 +0530

    Updated Routes

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -7,8 +7,8 @@ import { CartProvider as USCProvider } from "use-shopping-cart"
 export default function CartProvider({children} : {children: ReactNode}) {
     return (
         <USCProvider mode="payment" cartMode="client-only" stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-        successUrl="http://localhost:3000/stripe/success"
-        cancelUrl="http://localhost:3000/stripe/error"
+        successUrl="https://shopify-git-main-leela-manohar-gudivadas-projects.vercel.app/stripe/success"
+        cancelUrl="https://shopify-git-main-leela-manohar-gudivadas-projects.vercel.app/stripe/error"
         currency="USD"
         billingAddressCollection={true}
         shouldPersist={true}
